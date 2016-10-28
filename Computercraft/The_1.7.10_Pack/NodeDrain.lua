8d45b97ee1eee080386c6760728ce2b0e9a23555
sir_rigel <sir.rigel@gmail.com>
2016-10-28 12:54:53 -0500

    just some cleanup, to make the code look a bit nicer.

--- Computercraft/The_1.7.10_Pack/NodeDrain.lua
+++ Computercraft/The_1.7.10_Pack/NodeDrain.lua
@@ -1,10 +1,11 @@
 redstone_side="bottom"
 redstone_input="front"
 while(not redstone.getInput(redstone_input)) do
-	node_aspects=peripheral.call("top","getAspects")
+	node=peripheral.wrap("top")
+	node_aspects=node.getAspects()
 	startDrain=true
  for i=1,table.getn(node_aspects) do
-		startDrain=peripheral.call("top","getAspectCount",node_aspects[i])>1 and startDrain
+	 	startDrain=node.getAspectCount(node_aspects[i])>1 and startDrain
  end
  if startDrain then
    redstone.setOutput(redstone_side,true)
